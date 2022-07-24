5e3bc86a80ef27ac858f3b318f8883e54cf04194
Renata Yakobov <rinat.yuri@gmail.com>
2022-07-24 13:39:17 -0400

    Added edit message functionality

--- js/index.js
+++ js/index.js
@@ -13,18 +13,78 @@ for (i=0; i<skills.length; i++) {
     skillsList.appendChild(skill); 
 
 }
+//select element by name
 const messageForm = document.querySelector('[name="leave_message"]');
-console.log(messageForm.innerHTML.trim());
+const nameInput = messageForm.querySelector('[name="name"]');
+const emailInput = messageForm.querySelector('[name="email"]');
+const messageInput = messageForm.querySelector('[name="message"]');
+const messageSection = document.getElementById('messages');
+const messageList = messageSection.querySelector('ul');
+const removeButton = document.createElement ('button');
+const editButton = document.createElement ('button');
+
+function hide () {
+    if (messageList.childElementCount == 0) {
+        messageSection.style.display= "none";
+    } else {
+        messageSection.style.display= "block";
+    }
+}
+
+hide();
+//add a new message
 messageForm.addEventListener('submit',(e) => {
  e.preventDefault();
+ const nameInputValue = nameInput.value;
+ const emailInputValue = emailInput.value;
+ const messageInputValue = messageInput.value;
+ const newMessage = document.createElement ('li');
+ newMessage.innerHTML = `<a href = mailto: ${emailInputValue}> ${nameInputValue} </a> wrote: <span> ${messageInputValue} </span>`;
+ //const removeButton = document.createElement ('button');
+ removeButton.textContent = 'remove';
+ removeButton.type = 'button'
+ newMessage.appendChild(removeButton)
+ removeButton.addEventListener('click', (e) => {
+    let entry = e.target.parentNode;
+    entry.remove(); 
+    hide () ;
+})
+
+editButton.textContent = 'edit';
+editButton.type = 'button';
+newMessage.appendChild(editButton)
+
+editButton.addEventListener('click', (e) => {
+    if (editButton.textContent==="edit") {
+        editButton.textContent = 'save';
+        //get reference to the li node of the message
+        const li = e.target.parentNode;
+        //get reference to the span node within li
+        const span = li.querySelector('span');
+        //create an input element 
+        const input = document.createElement('input');
+        input.value = span.textContent;
+        li.insertBefore(input,span);
+        span.remove();
+        
+    } else if (editButton.textContent==="save") {
+        editButton.textContent = 'edit'; 
+        const li = e.target.parentNode;
+        const input = li.querySelector('input');
+        const span = document.createElement('span');
+        span.textContent = input.value + ' ';
+        li.insertBefore(span,input);
+        input.remove();
+
+    }
+    
+})
+
+messageList.appendChild(newMessage);
+messageForm.reset();
+hide () ;
+
 
 });
-console.log(messageForm);
-console.log('Test message');
-console.log(today);
-console.log(thisYear);
-console.log(footer);
-console.log(copyright);
-console.log(skills);
-console.log(skillsSection);
-console.log(skillsList);
+
+
